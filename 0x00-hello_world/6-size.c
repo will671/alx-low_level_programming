3f16ccdf86e78073b0a3823c60a710c5f81a3e2f
will671 <willtreaty4333@gmail.com>
2022-06-03 03:25:16 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 
 int main(void){
@@ -10,12 +9,17 @@ int main(void){
 	long along;
 
 	long long grain;
+
 	float goat;
 
  	printf("Size of a char: %lu byte(s)\n", sizeof(funny));
+	
 	printf("Size of an int: %lu byte(s)\n", sizeof(huge));
+	
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(grain));
+	
 	printf("Size of a float: %lu byte(s)\n", sizeof(goat));
 
 
