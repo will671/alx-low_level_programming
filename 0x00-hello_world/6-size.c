bbdf03c8406229c3a5665eb854dd7e9f62b1af6c
will671 <willtreaty4333@gmail.com>
2022-06-03 03:32:39 -0700

    passes to compiler

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,18 @@
 #include<stdio.h>
 
-int main(void){
+int main(void)
+{
 	
 	char funny;
-
 	int huge;
-
-	long along;
-
+	long along
 	long long grain;
-
 	float goat;
-
- 	printf("Size of a char: %lu byte(s)\n", sizeof(funny));
-	
-	printf("Size of an int: %lu byte(s)\n", sizeof(huge));
-	
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(grain));
-	
+ 	printf("Size of a char: %lu byte(s)\n", sizeof(funny));	
+	printf("Size of an int: %lu byte(s)\n", sizeof(huge));	
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));	
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(grain));	
 	printf("Size of a float: %lu byte(s)\n", sizeof(goat));
-
-
 	return(0);
 	
 }
