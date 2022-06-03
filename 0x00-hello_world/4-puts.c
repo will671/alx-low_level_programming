f2d7a7e4b28a5a60e3578a0b4ef12c5a2317221d
will671 <willtreaty4333@gmail.com>
2022-06-03 02:17:46 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,7 +12,14 @@
  *
  *     */
 
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
